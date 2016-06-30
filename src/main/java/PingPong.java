48051d249de3e8923c59dd5b8da400b11c26ef16
machewgc <mattygcz@gmail.com>
2016-06-29 22:56:26 -0500

    passed first test

--- src/main/java/PingPong.java
+++ src/main/java/PingPong.java
@@ -4,8 +4,11 @@ public class PingPong {
   public static void main(String[] args) {
 
   }
-  public static ArrayList<Object> pingPong(Integer year) {
+  public static ArrayList<Object> pingPong(Integer number) {
     ArrayList<Object> pingPongResults = new ArrayList<Object>();
+    for (Integer i = 1; i <= number; i++) {
+      pingPongResults.add(i);
+    }
     return pingPongResults;
   }
 }
