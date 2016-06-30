d1ce8984caf33ccf9b5e45bd220dd6cc3aedbfce
machewgc <mattygcz@gmail.com>
2016-06-29 23:08:28 -0500

    added logic for number divisible by 5, passed test

--- src/main/java/PingPong.java
+++ src/main/java/PingPong.java
@@ -7,7 +7,9 @@ public class PingPong {
   public static ArrayList<Object> pingPong(Integer number) {
     ArrayList<Object> pingPongResults = new ArrayList<Object>();
     for (Integer i = 1; i <= number; i++) {
-      if (i % 3 == 0) {
+      if(i % 5 == 0) {
+        pingPongResults.add("pong");
+      } else if (i % 3 == 0) {
         pingPongResults.add("ping");
       } else {
         pingPongResults.add(i);
